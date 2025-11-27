070f506e90f52cd200f8df45a05165800a78b2e2
ileies <admin@ileies.de>
2025-11-27 11:46:15 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '/favicon.svg?url';
+    import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+	
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navbar/>
+{@render children()}
+<Footer/>
---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() },
+	compilerOptions: {
+		runes: true
+	},
+	vitePlugin: {
+		compilerOptions: {
+			runes: true
+		},
+		// prevent compiling deps
+		exclude: ['node_modules/**']
+	}
+};
+
+export default config;
